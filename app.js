6ff2ce079450861d58d93ca8637fff7a9bf9e18a
lidunlife <uchihalidun@gmail.com>
2024-04-11 19:35:46 +0500

    first js project

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = prompt("what is your name?")
+let surname = prompt("what is your name?")
+let age = prompt("How old are you?")
+let home = prompt("Where do you live?")
+let sub = prompt("What is your favorite subject?")
+
+console.log("Name: "+ name);
+console.log("Surname: "+ surname);
+console.log("Age : "+ age);
+console.log("Location : "+  home);
+console.log("Favorite subject : "+  sub);
